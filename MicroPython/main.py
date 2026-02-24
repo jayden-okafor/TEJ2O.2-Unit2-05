6c91bd42e99b9c45f063f92032765ab94d553fb3
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-24 12:53:23 -0500

    يسيسيسبيسيسيسيس""""@@@@@

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jayden Okafor
+Created on: Feb 2026
+This module shows the temperature in celcius
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temp = temperature()
+        temp = (temp + 273.15)
+
+        display.clear()
+        display.scroll("The temperature is: " + str(temp) + " K")
+
+        display.clear()
+        
