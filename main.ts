581dd46c1736f82f18d98f9822c7a8a6b3bd720f
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-02-24 12:48:18 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jayden
+ * Created on: Feb 2026
+ * This program tells you the temperature in kelvin using the microbit
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen();
+basic.showIcon(IconNames.Happy)
+
+let temperature: number
+
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    temperature = (temperature + 273.15)
+
+    basic.clearScreen();
+    basic.showString('The temperature is: ' + temperature.toString() + ' K')
+})
+
+basic.clearScreen();
